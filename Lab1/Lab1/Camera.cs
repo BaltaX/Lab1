ad92718a48e913786a4bb887d2245fb1d9a125ec
BaltaX <kristian.sundstrom@agrifood.lu.se>
2012-11-13 14:50:33 +0100

    Upload  1

---
+++ Lab1/Lab1/Camera.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Lab1
+{
+    class Camera
+    {
+
+        internal Vector2 getViewCoordinatesWhite(int model_x, int model_y)
+        {
+            int sizeOfTile = 64;
+            int borderSize = 64;
+            int visualX = borderSize + model_x * sizeOfTile;
+            int visualY = borderSize + model_y * sizeOfTile;
+
+            return new Vector2(visualX, visualY);
+        }
+
+        internal Vector2 getViewCoordinatesBlack(int model_x, int model_y)
+        {
+            int sizeOfTile = 64;
+            int borderSize = 64;
+            int visualX = borderSize + (7-model_x) * sizeOfTile;
+            int visualY = borderSize + (7-model_y) * sizeOfTile;
+
+            return new Vector2(visualX, visualY);
+        }
+
+
+        internal Vector2 getViewCoordinatesMobileWhite(int model_x, int model_y)
+        {
+            int sizeOfTile = 24;
+            int borderSize = 24;
+            int visualX = borderSize + model_x * sizeOfTile;
+            int visualY = borderSize + model_y * sizeOfTile;
+
+            return new Vector2(visualX, visualY);
+        }
+
+
+    }
+}
